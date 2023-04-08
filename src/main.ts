1d264c588060242efa87aead4e3cabe59b89fa37
Jonathan Sarry <jsarry@gmail.com>
2023-04-08 12:41:09 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+// For more information, see https://crawlee.dev/
+import { PlaywrightCrawler,  Dataset } from 'crawlee';
+import { storeData } from './storeDB.ts';
+
+const startUrls = ['https://www.etuovi.com/myytavat-asunnot/helsinki/lauttasaari/taloyhtiot/0120209-3/asunto-oy-melkonkatu-3-bostads-ab'];
+
+const APARTMENT_LABEL = 'APARTMENT'
+
+const crawler = new PlaywrightCrawler({
+    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+    async requestHandler({ page, parseWithCheerio,log, request,enqueueLinks }) {
+        log.info(request.url);
+
+
+        if (request.label === APARTMENT_LABEL) {
+            await page.waitForSelector('.react-swipeable-view-container');
+            const $ = await parseWithCheerio();
+            const title = await page.title();
+
+            const url = request.url
+            const id = url.split('/').pop()
+
+            console.log(`The title of the apartmetn "${request.url}" is: ${title}.`)
+
+            const images: string[] = []
+
+
+            $('.react-swipeable-view-container img').each((i, el) => {
+               images.push(el.attribs.src)
+            })
+
+            console.log($('#infos h3').first().text())
+            
+            const dataCollected = {
+                etuovi_id: id,
+                title: title,
+                url: request.url,
+                images: images,
+                price: $('#infos h3').first().text()
+            }
+
+            Dataset.pushData(dataCollected);
+
+
+            storeData({
+                ...dataCollected,
+                screen_shot: await page.screenshot({fullPage: true})
+            });            
+        }
+        else {
+            await enqueueLinks({
+                globs: ['https://www.etuovi.com/kohde/*'],
+                label: APARTMENT_LABEL
+            })  
+        }
+
+    }
+});
+
+await crawler.run(startUrls);
---
+++ src/storeDB.ts
@@ -0,0 +1,47 @@
+import PocketBase from 'pocketbase';
+import { Blob } from 'buffer';
+import { randomUUID } from 'crypto';
+import axios from 'axios';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+interface ApartmentData {
+    title: string
+    url: string
+    images: string[]
+    price: number | string
+    screen_shot: Buffer
+    etuovi_id: string
+}
+
+export const storeData = async (data: ApartmentData) => {
+    // check if apartment exists
+    // store slide show images
+    pb.autoCancellation(false)
+
+    let imageIds : string[] = []
+
+    for (const image of data.images) {
+        const formData = new FormData()
+        formData.append('single', new Blob([await axios.get(image, { responseType: 'arraybuffer' }).then(res => res.data)]), randomUUID())
+
+        const imageId = await pb.collection('images').create(formData)
+        imageIds.push(imageId.id)
+    }
+
+
+    // store screenshot image
+    const formData = new FormData()
+    formData.append('single', new Blob([data.screen_shot]), randomUUID())
+
+    const screenShot = await pb.collection('images').create(formData)
+
+    console.log(imageIds)
+
+    // store apartment record w/ associated images
+    return await pb.collection('apartments').create({
+        ...data,
+        screen_shot: screenShot.id,
+        images: imageIds
+    })
+}
